18b00e8b7151bc2ea6cc21aa244eb3b1b003b60a
Renato Lewkowicz <renato.lew@gmail.com>
2021-08-07 16:39:26 -0300

    Built the post model, customized the admin display and created the first posts in the database.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # To do list:
-# Set up Project
-# Create database models
-# Create superuser to admin the site
-# Add models to admin
+
 # Build Views
 # Add URL patterns to views
 # Create template to views
