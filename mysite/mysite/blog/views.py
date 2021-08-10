9074ea1f5f2d09091724fa15a923ec8b2b2c8b5a
Renato Lewkowicz <renato.lew@gmail.com>
2021-08-10 17:19:21 -0300

    Fully adding comment forms and comment moderation.

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -1,13 +1,33 @@
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
-    model = Post
+def post_detail(request, slug):
     template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
 
+    # Comment is posted.
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
 
+            # Create comment object without commiting it to save in database.
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment.
+            new_comment.post = post
+            # Save post in database.
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
+    return render(request, template_name, { 'post': post,
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
