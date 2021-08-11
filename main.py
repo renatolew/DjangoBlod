463ef858197c0aab75fce2a429a4614f9bddd785
Renato Lewkowicz <renato.lew@gmail.com>
2021-08-11 13:40:27 -0300

    Updating To Do List.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 # To do list:
 
-# Create a model to save comments
-# Create a form to submit comments and validate the input data
-# Add a view that processes the form and saves the new comment to the database
-# Edit the post detail template to display the list of comments and the form to add a new comment
+# Project done!!
